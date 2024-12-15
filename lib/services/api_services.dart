712e47d857b8c6c662ee42939cd2fe6da3c0e78d
MladenovStefan <59606992+MladenovStefan@users.noreply.github.com>
2024-12-15 20:29:15 +0100

    deleted comments

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -13,21 +13,6 @@ class ApiService {
       throw Exception("Failed to fetch joke types");
     }
   }
-
-  // Fetch jokes by type
-/*************  ✨ Codeium Command ⭐  *************/
-  /// Fetches a list of jokes of the specified type from the API.
-  ///
-  /// The function sends a GET request to the API endpoint corresponding
-  /// to the given joke type and retrieves up to ten jokes of that type.
-  ///
-  /// Returns a Future that resolves to a List of Maps, where each Map
-  /// represents a joke with its associated data.
-  ///
-  /// Throws an Exception if the request fails or if the server response
-  /// is not successful.
-
-/******  5f3afeda-efe3-4adb-b8c1-2d364fc117a4  *******/
   static Future<List<Map<String, dynamic>>> fetchJokesByType(String type) async {
     final response = await http.get(Uri.parse('$baseUrl/jokes/$type/ten'));
     if (response.statusCode == 200) {
